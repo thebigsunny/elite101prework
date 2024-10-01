89a223c16b07cb70e2de72e1125706c6c68a06fa
thebigsunny <shivaangr@gmail.com>
2024-09-30 22:00:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Welcome to the Elite 101 Chat Bot!")
+
+def intro():
+    username = input("What is your name?: ")
+    print("Nice to meet you, " + username)
+    age = input("How old are you?: ")
+    print("Welcome " + username + "! " + "Oh to be " + age + " again... " + "How can I help you today?")
+    print("Please choose from the following options: ")
+    print("1. Placeholder Option 1")
+    print("2. Placeholder Option 2")
+    print("3. Placeholder Option 3")
+    print("4. Exit the conversation")
+
+    pagenumber = input("Enter the number of your choice: ")
+
+intro()
